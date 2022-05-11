31c112bb978064db5427a66401ad7f15c8011709
Stas Pochynok <staspochyn@gmail.com>
2022-05-10 22:23:40 -0400

    types

--- components/widgets/folder2/widget3.tsx
+++ components/widgets/folder2/widget3.tsx
@@ -1,18 +1,20 @@
 import React from "react";
 import { ResponsiveContainer } from "recharts";
+import { Widget } from '../../../utils/types';
 
-const Widget1 = () => {
+
+const Widget3 = () => {
   return (
     <ResponsiveContainer width="99%" height="99%">
-      <div>widget1</div>
+      <div>widget3</div>
     </ResponsiveContainer>
   );
 };
 
-const chartWidget = {
-  component: <Widget1 />,
-  key: "chart1",
-  label: "Chart 1",
+const chartWidget: Widget = {
+  component: <Widget3 />,
+  key: "chart3",
+  label: "Chart 3",
 };
 
 export default chartWidget;

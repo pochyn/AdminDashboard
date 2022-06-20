1d5f82ecf4bcea7d0165aab53fd003d4a5e638c6
Stas <staspochyn@gmail.com>
2022-06-19 21:43:10 -0400

    check

--- win-push.cmd
+++
@@ -1,7 +0,0 @@
-docker login --username AWS --password $(aws ecr get-login-password 2>&1) 883846757755.dkr.ecr.us-east-1.amazonaws.com
-
-docker build -t dashboard-tool .
-
-docker tag dashboard-tool:latest 883846757755.dkr.ecr.us-east-1.amazonaws.com/dashboard-tool:latest
-
-docker push 883846757755.dkr.ecr.us-east-1.amazonaws.com/dashboard-tool:latest
